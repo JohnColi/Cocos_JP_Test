f168f6dafe5df39b0698cc44b7cb4fc73b4da1c0
CALDAWIN\Johnny.Lee <>
2024-09-02 18:23:43 +0800

    多主題bundle 測試

--- assets/Scriptes/LoadTheme.ts
+++ assets/Scriptes/LoadTheme.ts
@@ -1,14 +1,39 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Canvas, Component, instantiate, Node, Prefab, sp } from 'cc';
+import { assetManager } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('LoadTheme')
 export class LoadTheme extends Component {
-    start() {
+    @property(sp.Skeleton)
+    JP_ske: sp.Skeleton;
 
+    @property(Node)
+    jp_ske_parent: Node;
+
+    start() {
+        window.api = new Object()
+        window.api.loadGreen = this.Loadbundle.bind(this);
     }
 
     Loadbundle() {
-
+        let url = "https://prd10-icontent.calda.win/lax/7001/assets/Green";
+        console.log("load bundle:", url);
+        assetManager.loadBundle(url, (err, bundle) => {
+            if (err) {
+                console.error(err);
+            } else {
+                bundle.load(`prefab`, Prefab, function (err, prefab) {
+                    if (err) {
+                        console.error("prefab is error,", err);
+                    } else {
+                        let obj = instantiate(prefab);
+                        let _ske = obj.getComponent(sp.Skeleton);
+                        this.JP_ske = _ske;
+                        this.jp_ske_parent.insertChild(obj, 1);
+                    }
+                });
+            }
+        });
     }
 
     update(deltaTime: number) {
